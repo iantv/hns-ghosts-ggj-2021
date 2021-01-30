171a53ce74e8d6bbd5de7ddb51a318d172825012
iantv <ian.tatiana.v@gmail.com>
2021-01-30 18:20:38 +1000

    Add textures to floor and walls

--- Assets/Scripts/PropsGenerator.cs
+++ Assets/Scripts/PropsGenerator.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PropsGenerator : MonoBehaviour
 {
-    private GameObject []Walls;
+    private List<GameObject> Walls;
     public GameObject PaintingPrefab;
     void Start()
     {
-        Walls = GameObject.FindGameObjectsWithTag("Wall");
-
+        Walls = new List<GameObject>();
+        Walls.AddRange(GameObject.FindGameObjectsWithTag("Wall"));
         foreach (var wall in Walls)
         {
             int r = Random.Range(0, 4);
