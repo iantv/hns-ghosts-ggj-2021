cd64538fe68c83e90299c70cae058c8e1a2a574b
iantv <ian.tatiana.v@gmail.com>
2021-01-31 03:22:08 +1000

    Generate props for bathroom

--- Assets/Scripts/PropsGenerator.cs
+++ Assets/Scripts/PropsGenerator.cs
@@ -6,21 +6,55 @@ using UnityEngine;
 public class PropsGenerator : MonoBehaviour
 {
     private List<GameObject> Walls;
-    public GameObject PaintingPrefab;
+    private Dictionary<string, List<GameObject>> Prefabs = new Dictionary<string, List<GameObject>>();
+
+    public string SpaceName;
+
     void Start()
     {
+        InitInteriorPrefabs();
+
         Walls = new List<GameObject>();
-        Walls.AddRange(GameObject.FindGameObjectsWithTag("Wall"));
+        Transform GeometryItems = transform.Find("Geometry");
+        foreach (Transform ch in GeometryItems)
+        {
+            if (ch.gameObject.CompareTag("Wall"))
+                Walls.Add(ch.gameObject);
+        }
+
         foreach (var wall in Walls)
         {
-            int r = Random.Range(0, 4);
-            if (r % 4 != 0) continue;
-            var prop = Instantiate(PaintingPrefab, wall.transform);
+            Debug.Log("Comparison: SpaceName is " + SpaceName);
+            if (string.Compare(SpaceName, "RoomSpace") == 0)
+            {
+                int r = Random.Range(0, 8);
+                GameObject bathItem = Prefabs[SpaceName][r % Prefabs[SpaceName].Count];
+                var b = Instantiate(Prefabs[SpaceName][r % Prefabs[SpaceName].Count], wall.transform.position, wall.transform.rotation, gameObject.transform);
+                b.transform.Translate(wall.transform.right * 0.5f, Space.World);
+            } else if (string.Compare(SpaceName, "CoridorSpace") == 0)
+            {
+                SpawnPainting(wall.transform);
+            } else
+            {
+                Debug.Log("SpaceName is " + SpaceName);
+            }
         }
     }
 
-    void Update()
+    void InitInteriorPrefabs()
     {
+        List<GameObject> TempPrefabs = new List<GameObject>();
+        TempPrefabs.Add(Resources.Load("Models/Props/Painting") as GameObject);
+        Prefabs.Add("CoridiorSpace", TempPrefabs);
 
+        TempPrefabs.Clear();
+        TempPrefabs.Add(Resources.Load("Models/Props/Bath") as GameObject);
+        TempPrefabs.Add(Resources.Load("Models/Props/Sink") as GameObject);
+        Prefabs.Add("RoomSpace", TempPrefabs);
+    }
+
+    void SpawnPainting(Transform wall)
+    {
+        var prop = Instantiate(Prefabs[SpaceName][0], wall.transform);
     }
 }
