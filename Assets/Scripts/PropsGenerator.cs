91a083e96d316c05837a0649630f166d74360aea
iantv <ian.tatiana.v@gmail.com>
2021-01-31 04:41:25 +1000

    Add Bathroom items

--- Assets/Scripts/PropsGenerator.cs
+++ Assets/Scripts/PropsGenerator.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class PropsGenerator : MonoBehaviour
 {
     private List<GameObject> Walls;
+    private GameObject Floor;
     private Dictionary<string, List<GameObject>> Prefabs = new Dictionary<string, List<GameObject>>();
 
     public string SpaceName;
-
+    private string Subplace = "Bathroom";
     void Start()
     {
         InitInteriorPrefabs();
@@ -20,17 +21,24 @@ public class PropsGenerator : MonoBehaviour
         {
             if (ch.gameObject.CompareTag("Wall"))
                 Walls.Add(ch.gameObject);
+            else if (ch.gameObject.CompareTag("Floor"))
+            {
+                Floor = ch.gameObject;
+            }
         }
 
         foreach (var wall in Walls)
         {
-            Debug.Log("Comparison: SpaceName is " + SpaceName);
             if (string.Compare(SpaceName, "RoomSpace") == 0)
             {
                 int r = Random.Range(0, 8);
                 GameObject bathItem = Prefabs[SpaceName][r % Prefabs[SpaceName].Count];
                 var b = Instantiate(Prefabs[SpaceName][r % Prefabs[SpaceName].Count], wall.transform.position, wall.transform.rotation, gameObject.transform);
                 b.transform.Translate(wall.transform.right * 0.5f, Space.World);
+
+                Floor.GetComponent<MeshRenderer>().material = Resources.Load("Materials/BathroomFloor") as Material;
+                wall.GetComponent<MeshRenderer>().material = Resources.Load("Materials/BathroomWall") as Material;
+
             } else if (string.Compare(SpaceName, "CoridorSpace") == 0)
             {
                 SpawnPainting(wall.transform);
